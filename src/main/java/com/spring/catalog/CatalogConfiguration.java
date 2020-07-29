7b1b3ae79b1121731bafe0108d66bfb90ca75684
VSENTH17 <VSENTH17@ford.com>
2020-07-29 17:58:47 +0530

    Added Configuration for ProductInventory Service

--- src/main/java/com/spring/catalog/CatalogConfiguration.java
+++ src/main/java/com/spring/catalog/CatalogConfiguration.java
@@ -1,8 +1,12 @@
 package com.spring.catalog;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import com.spring.catalog.service.InventoryService;
+import com.spring.catalog.service.ProductInventoryService;
 
 @Configuration
 public class CatalogConfiguration {
@@ -10,5 +14,11 @@ public class CatalogConfiguration {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	@Qualifier("productinventory")
+	public InventoryService createProductInventoryService(RestTemplate restTemplate) {
+		return new ProductInventoryService(restTemplate);
 	}
 }
